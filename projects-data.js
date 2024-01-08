9a9e3bbc09203dd3c0388047a835c7ce4f381075
Adam <adam.shelley18@gmail.com>
2024-01-08 18:59:56 +0000

    Removed excess projects

--- projects-data.js
+++ projects-data.js
@@ -18,21 +18,21 @@ export const projectData = [
       "With the success of Wordle, many have tried making their own small addictive game. This is my version. I used NextJS to handle the frontend and Tailwind for the styling. I used Prisma to link to a postgres database hosted on Railway. The database is automatically reset daily using a cron job.  ",
     learnings: "First time using prisma to link to a postgres database.",
   },
-  {
-    name: "Samantha Foo Pottery",
-    liveLink: "https://sam-foo-pottery.vercel.app/",
-    image: "./src/assets/pottery-screenshot.png",
-    imageName: "pottery-screenshot",
-    github: "https://github.com/AdamShelley/Samantha-Foo-Pottery",
-    npm: "",
-    skills: ["ReactJS", "ThreeJS", "GSAP", "Blender"],
-    screenshots: ["pottery1.png", "pottery2.png", "pottery3.png"],
-    description: "A portfolio website for Samantha Foo.",
-    background:
-      "This portfolio is built with ReactJS, and ThreeJs to handle the 3D model that I made in Blender of Samantha's studio. I used the GSAP animation package to handle the movement of the 3D model and HTML around the screen.",
-    learnings:
-      "This was my first deep dive into combining ReactJS with ThreeJS. I also honed my skills in 3D modeling using Blender and discovered the potential of GSAP for enhancing user interaction through animations",
-  },
+  // {
+  //   name: "Samantha Foo Pottery",
+  //   liveLink: "https://sam-foo-pottery.vercel.app/",
+  //   image: "./src/assets/pottery-screenshot.png",
+  //   imageName: "pottery-screenshot",
+  //   github: "https://github.com/AdamShelley/Samantha-Foo-Pottery",
+  //   npm: "",
+  //   skills: ["ReactJS", "ThreeJS", "GSAP", "Blender"],
+  //   screenshots: ["pottery1.png", "pottery2.png", "pottery3.png"],
+  //   description: "A portfolio website for Samantha Foo.",
+  //   background:
+  //     "This portfolio is built with ReactJS, and ThreeJs to handle the 3D model that I made in Blender of Samantha's studio. I used the GSAP animation package to handle the movement of the 3D model and HTML around the screen.",
+  //   learnings:
+  //     "This was my first deep dive into combining ReactJS with ThreeJS. I also honed my skills in 3D modeling using Blender and discovered the potential of GSAP for enhancing user interaction through animations",
+  // },
   {
     name: "react-onboarder-tool",
     liveLink: "https://onboarder-demonstration.web.app/",
@@ -48,26 +48,26 @@ export const projectData = [
     learnings:
       "Creating this tool was a lesson in package development and distribution. It was my first experience publishing to NPM, and I learned a lot about the process of creating a package, documenting and versioning it in an understandable way.",
   },
-  {
-    name: "Scribbler",
-    liveLink: "https://scribbler-app.com/",
-    image: "./src/assets/scribbler-screenshot.png",
-    imageName: "scribbler-screenshot",
-    github: "https://github.com/AdamShelley/scribbler",
-    npm: "",
-    description:
-      "A online markdown editor that organises your notes and keeps them stored.",
-    skills: ["React", "Firebase", "Auth"],
-    screenshots: [
-      "scribbler3.png",
-      "scribbler4.png",
-      "scribbler5.png",
-      "scribbler6.png",
-      "scribbler7.png",
-      "scribbler2.jpg",
-    ],
-    background: `Motivated by a desire to create a practical, everyday tool, I built Scribbler using React for frontend development, and Firebase for backend storage.`,
-    learnings:
-      "Enhanced my understanding of Firebase deployment, email authentication, and GitHub sign-in integration.",
-  },
+  // {
+  //   name: "Scribbler",
+  //   liveLink: "https://scribbler-app.com/",
+  //   image: "./src/assets/scribbler-screenshot.png",
+  //   imageName: "scribbler-screenshot",
+  //   github: "https://github.com/AdamShelley/scribbler",
+  //   npm: "",
+  //   description:
+  //     "A online markdown editor that organises your notes and keeps them stored.",
+  //   skills: ["React", "Firebase", "Auth"],
+  //   screenshots: [
+  //     "scribbler3.png",
+  //     "scribbler4.png",
+  //     "scribbler5.png",
+  //     "scribbler6.png",
+  //     "scribbler7.png",
+  //     "scribbler2.jpg",
+  //   ],
+  //   background: `Motivated by a desire to create a practical, everyday tool, I built Scribbler using React for frontend development, and Firebase for backend storage.`,
+  //   learnings:
+  //     "Enhanced my understanding of Firebase deployment, email authentication, and GitHub sign-in integration.",
+  // },
 ];
